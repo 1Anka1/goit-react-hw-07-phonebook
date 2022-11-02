5a5593bb0ec61921a6241c71d95ce3a264f1019f
Anna Kasianenko <97912793+1Anka1@users.noreply.github.com>
2022-11-02 16:41:17 +0100

    Update

--- src/redux/contacts/operations.js
+++ src/redux/contacts/operations.js
@@ -1,10 +1,10 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
-//
+// import { createAsyncThunk } from '@reduxjs/toolkit';
+// import axios from 'axios';
+// //
 
-axios.defaults.baseURL = 'https://63626f9a66f75177ea2f79a4.mockapi.io';
+// axios.defaults.baseURL = 'https://63626f9a66f75177ea2f79a4.mockapi.io';
 
-export const fetchContacts = createAsyncThunk('tasks/fetchAll', async () => {
-  const response = await axios.get('/contacts');
-  return response.data;
-});
+// export const fetchContacts = createAsyncThunk('tasks/fetchAll', async () => {
+//   const response = await axios.get('/contacts');
+//   return response.data;
+// });
