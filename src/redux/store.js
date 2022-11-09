5bcb1165948cf963fc05b9f3839bc0e3da944c89
Anna Kasianenko <97912793+1Anka1@users.noreply.github.com>
2022-11-09 23:06:51 +0100

    Delete persistGate

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,33 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 
-//REDUX
-import rootReducer from './rootReducer';
+import contactReducer from './contacts/slice';
+import filterReducer from './filter/slice';
 
-const contactsPersistConfig = {
-  key: 'root',
-  storage,
-  blacklist: ['filter'],
-};
-
-const persistedReducer = persistReducer(contactsPersistConfig, rootReducer);
-export const store = configureStore({
-  reducer: persistedReducer,
-  middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+const store = configureStore({
+  reducer: {
+    contacts: contactReducer,
+    filter: filterReducer,
+  },
 });
-export const persistor = persistStore(store);
+
+export default store;
